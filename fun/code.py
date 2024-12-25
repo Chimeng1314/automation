e5dc225b7b45fa42e01357db25b5ae2ac2f48aa2
Dream <2068946849@qq.com>
2024-12-25 15:46:25 +0800

    Update code.py
    修改打码链接

--- fun/code.py
+++ fun/code.py
@@ -5,7 +5,7 @@ import base64
 
 # 识别验证码
 def identify(mo,code,model):
-    url = f"http://cn-hk-bgp-4.ofalias.net:50818/inference_wps_{mo}"
+    url = f"http://localhost:18880/inference_wps_{mo}"
     payload = code
     headers = {
         'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
